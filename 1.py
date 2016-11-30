7551f205c90da9152a2e77ba29afbe2fdf31d9e3
bereczb <balazs.berecz@gmail.com>
2016-11-30 18:11:06 +0100

    task1_completed

--- 1.py
+++ 1.py
@@ -1,3 +1,16 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
+
+def double_elements(input_list):
+
+    if type(input_list) is not list:
+        raise TypeError('The parameter is not a list!')
+
+    result_list = []
+
+    for i in input_list:
+        result_list.append(i * 2)
+    return result_list
+
+print(double_elements([1, 2, 3, 4, 5]))
